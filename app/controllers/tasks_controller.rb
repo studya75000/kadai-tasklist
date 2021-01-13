daec1ca2b6c5ce635ce4503b405ec651d46cf9ad
study.a75000 <study.a75000@gmail.com>
2021-01-13 08:32:34 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
       @tasks = Task.all
@@ -52,7 +52,7 @@ class TasksController < ApplicationController
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content :status)
+    params.require(:task).permit(:content, :status)
   end
   
 end
